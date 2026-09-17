65c36cef7913e0063f3b7d41f21d1463d1479ecc
Alexander Joham <alex.joham@tum.de>
2026-09-17 11:19:24 +0200

    Test import size_t without <cstddef>

---
+++ include/tm/constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <initializer_list>
+
+namespace tm_core {
+
+constexpr std::size_t kMaxFrameSize = 256;
+
+} // namespace tm_core
--- include/tm/core.hpp
+++ include/tm/core.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "tm/constants.hpp"
+
 namespace tm_core {
 
 class Core {
