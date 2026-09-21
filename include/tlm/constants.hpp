8cba29a0dad40073cea1add3608869531ea6c402
Alexander Joham <alex.joham@tum.de>
2026-09-21 12:08:24 +0200

    Set all constants in constants.hpp to inline constexpr

--- include/tlm/constants.hpp
+++ include/tlm/constants.hpp
@@ -7,16 +7,16 @@
 
 namespace tlm {
 
-constexpr std::byte kSyncByte0{0xA5};
-constexpr std::byte kSyncByte1{0xC3};
+inline constexpr std::byte kSyncByte0{0xA5};
+inline constexpr std::byte kSyncByte1{0xC3};
 
-constexpr std::size_t kLengthFieldOffset = 10;
+inline constexpr std::size_t kLengthFieldOffset = 10;
 
-constexpr std::size_t kFixedHeaderSize = 12;
-constexpr std::size_t kCrcSize = 2;
+inline constexpr std::size_t kFixedHeaderSize = 12;
+inline constexpr std::size_t kCrcSize = 2;
 
-constexpr std::size_t kMaxPayloadSize = std::numeric_limits<std::uint8_t>::max();
-constexpr std::size_t kMaxFrameSize = kFixedHeaderSize + kMaxPayloadSize + kCrcSize;
+inline constexpr std::size_t kMaxPayloadSize = std::numeric_limits<std::uint8_t>::max();
+inline constexpr std::size_t kMaxFrameSize = kFixedHeaderSize + kMaxPayloadSize + kCrcSize;
 
 } // namespace tlm
 
